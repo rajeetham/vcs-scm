be2c124833d235e0dcc635d391c9281dfaf506bc
rajeetham <rajeetha0689@gmail.com>
2023-09-16 00:25:08 +0530

    file

---
+++ iac/main.tf
@@ -0,0 +1,50 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+Spot instance example
+data "aws_ami" "this" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
+  }
+}
+
+resource "aws_instance" "this" {
+  ami = data.aws_ami.this.id
+  instance_market_options {
+    spot_options {
+      max_price = 0.0031
+    }
+  }
+  instance_type = "t4g.nano"
+  tags = {
+    Name = "test-spot"
+  }
+}
